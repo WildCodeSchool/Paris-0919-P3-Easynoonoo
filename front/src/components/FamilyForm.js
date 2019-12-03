1860898b0f44437dbfc448191d7b2b49f258594e
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-12-03 11:07:23 +0100

    created FamilyForm component

---
+++ front/src/components/FamilyForm.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+const FamilyForm = () => {
+
+    const [question, setQuestion] = useState([]);
+
+    return(
+        <div>
+            Le son de la pitite
+        </div>
+    )
+}
+
+export default FamilyForm
