3df0e63d902e7aadff0c8aa4e1e571dc4f58b5ca
Barbara Paul <barbara.kikou@gmail.com>
2019-12-04 16:44:43 +0100

    added different conditions to show the questions

--- front/src/components/FamilyForm.js
+++ front/src/components/FamilyForm.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 
 const FamilyForm = () => {
 
+// state en hook pour les réponses
+
     const [answers1, setAnswers1] = useState('')
     const [answers2, setAnswers2] = useState()
     const [answers3, setAnswers3] = useState()
@@ -9,13 +11,22 @@ const FamilyForm = () => {
 
     return(
         <div>
+            {/* question 1 toujours visible + envoi de la valeur dans le state answers1 */}
+
             <p className='question1'>Combien d'enfants sont gardés par la nounou ?</p>
-            <input type = 'text'classname='answers1' value = {answers1} onChange = {e => setAnswers1(e.target.value)} min = "1" max = "10"/>
+            <input type = 'text'classname='answers1' value = {answers1} onChange = {e => setAnswers1(e.target.value)} min = '1' max = '10'/>
 
-            {answers1 !== '1' && answers1 !== '' ? <div><p className='question2'>Combien sont à vous ?</p> <input type = 'text'classname='answers2' value = {answers2} onChange = {e => setAnswers2(e.target.value)} min = "1" max = "10"/> </div> : console.log('nope')}
+            {/* si on a plus d'un enfant question 2 apparait */}
+
+            {answers1 > '1' ? <div><p className='question2'>Combien sont à vous ?</p> <input type = 'text'classname='answers2' value = {answers2} onChange = {e => setAnswers2(e.target.value)} min = "1" max = "10"/> </div> : console.log('nope')}
+
+            {/* si l'utilisateur a mis plus d'enfants à lui que d'enfants gardés = message d'erreur1 */}
             
+            {answers2 > answers1 ? <p>No trolling please !!</p> : console.log('ok')}
 
-            <p className='question3'>L'enfant est-il en garde partagé avec l'autre parents ?</p>
+
+            {answers2 < answers1 ? <p>Boum planning</p> : 
+            <div><p className='question3'>L'enfant est-il en garde partagé avec l'autre parents ?</p>
             <div className="radio">
           <label>
             <input type="radio" value="oui" checked={answers3 === 'oui'} onChange = {e => setAnswers3(e.target.value)} />
@@ -24,12 +35,15 @@ const FamilyForm = () => {
         </div>
         <div className="radio">
           <label>
-            <input type="radio" value="non" checked={answers2 === 'non'} onChange = {e => setAnswers3(e.target.value)} />
+            <input type="radio" value="non" checked={answers3 === 'non'} onChange = {e => setAnswers3(e.target.value)} />
             Non
           </label>
 
         </div>
+    </div>}
         {answers1 === '1' && answers3 === 'non' ?<p className='error1'>Tu payes tout lol</p> : console.log('lol')}
+        {answers1 === answers2 && answers3 === 'non' ? <p>Tu payes tout lol</p> : console.log('jaaj')}
+        {answers1 === answers2 && answers3 === 'oui' || answers1 === '1' && answers3 === 'oui' ? <p>Boum planning</p> : console.log('joj')}
 
         </div>
     )
