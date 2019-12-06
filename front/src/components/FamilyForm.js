d62460126857e733950b536abd78663d00eddbc4
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-12-06 11:56:08 +0100

    added comments

--- front/src/components/FamilyForm.js
+++ front/src/components/FamilyForm.js
@@ -8,50 +8,76 @@ const FamilyForm = () => {
     const [answers2, setAnswers2] = useState(0)
     const [answers3, setAnswers3] = useState()
 
-    useEffect(()=>{
-      console.log('useEffect', answers2, typeof(answers2));
-      
-    },[answers2])
+  
 
     return(
         <div>
-            {/* question 1 toujours visible + envoi de la valeur dans le state answers1 */}
+            {/* question 1 toujours visible + envoi de la valeur dans le state answers1 + converti la valeur obtenue en number*/}
 
             <p className='question1'>Combien d'enfants sont gardés par la nounou ?</p>
             <input type = 'number'classname='answers1' value = {answers1} onChange = {e => setAnswers1(parseInt(e.target.value, 10))} min = '1' max = '10' onClick = {e => setAnswers2(0)} />
 
             {/* si on a plus d'un enfant question 2 apparait */}
 
-            {answers1 > 1 ? <div><p className='question2'>Combien sont à vous ?</p> <input type = 'number'classname='answers2' value = {answers2} onChange = {e => setAnswers2(parseInt(e.target.value, 10))} min = "1" max = {answers1}/> </div> : console.log('nope')}
+            {answers1 > 1 ? <div><p className='question2'>Combien sont à vous ?</p> <input type = 'number'classname='answers2' value = {answers2} onChange = {e => setAnswers2(parseInt(e.target.value, 10))} min = "1" max = {answers1} onClick = {e => setAnswers3('')}/> </div> : console.log('nope')}
             
-            
-        {/* question 3 avec un radio check oui/non : -si on est en garde partagée avec une co-famille le calendrier apparait
-        -si on a le même nb d'enfant dans question 1 et 2 = vérification si garde partagée avec un ex */}
+             
+            {/* question 3 avec un radio check oui/non : garde partagée avec ex si plusieurs enfants */}
 
-            {answers2 < answers1 && answers2 !== 0 ? <p>Boum planning</p> : 
-            <div><p className='question3'>L'enfant est-il en garde partagé avec l'autre parents ?</p>
+            { answers1 === answers2 && answers1 > 1 ? 
+              <div>
+                <p className='question3'>Les enfants sont-ils en garde partagée avec l'autre parent ?</p>
+                <div className="radio">
+                  <label>
+                    <input type="radio" value="oui" checked={answers3 === 'oui'} onChange = {e => setAnswers3(e.target.value)} />
+                    Oui
+                  </label>
+                </div>
+
+                <div className="radio">
+                  <label>
+                    <input type="radio" value="non" checked={answers3 === 'non'} onChange = {e => setAnswers3(e.target.value)} />
+                    Non
+                   </label>
+                </div>
+              
+              </div>
+              : console.log('jij')              
+            }
+
+            {/* question 3 avec un radio check oui/non : garde partagée avec ex si un enfant */}
+
+            { answers1 == 1 ? 
+            <div>
+            <p className='question3'>L'enfant est-il en garde partagée avec l'autre parent ?</p>
             <div className="radio">
-          <label>
-            <input type="radio" value="oui" checked={answers3 === 'oui'} onChange = {e => setAnswers3(e.target.value)} />
-            Oui
-          </label>
-        </div>
-        <div className="radio">
-          <label>
-            <input type="radio" value="non" checked={answers3 === 'non'} onChange = {e => setAnswers3(e.target.value)} />
-            Non
-          </label>
+              <label>
+                <input type="radio" value="oui" checked={answers3 === 'oui'} onChange = {e => setAnswers3(e.target.value)} />
+                Oui
+              </label>
+            </div>
 
-        </div>
-    </div>}
+            <div className="radio">
+              <label>
+                <input type="radio" value="non" checked={answers3 === 'non'} onChange = {e => setAnswers3(e.target.value)} />
+                Non
+               </label>
+            </div>
+          
+          </div>
+          : console.log('yeh')
+            }
+    
 
-    {/* si on a 1 ou plusieurs enfant et pas de partage avec ex ni co-partage = erreur  */}
-        {answers1 === 1 && answers3 === 'non' ?<p className='error1'>Tu payes tout lol</p> : console.log('lol')}
-        {answers1 === answers2 && answers3 === 'non' ? <p>Tu payes tout lol</p> : console.log('jaaj')}
+    {/* Message erreur si pas garde partagée  */}
 
-    {/* si on est en co-partage avec l'ex = planning apparait */}
+        {answers1 === 1 && answers3 === 'non' ?<p className='error1'>La garde de l'enfant n'étant pas partagée, l'intégralité des coût de celle-ci est à votre charge</p> : console.log('lol')}
+        {answers1 === answers2 && answers3 === 'non' ? <p>La garde des enfants n'étant pas partagée, l'intégralité des coût de celle-ci est à votre charge</p> : console.log('jaaj')}
+
+    {/* si on est en co-partage : planning apparait */}
 
         {answers1 === answers2 && answers3 === 'oui' || answers1 === 1 && answers3 === 'oui' ? <p>Boum planning</p> : console.log('joj')}
+        {answers2 < answers1 && answers2 !== 0  ? <p>Boum planning</p> : console.log('tofl') }
 
         </div>
     )
