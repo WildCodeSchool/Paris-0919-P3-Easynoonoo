38241be5ed83a754ab80fa61054d92f13f5c81c7
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-12-09 11:53:58 +0100

    created header component + started to add flexbox properties + style

---
+++ front/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+
+    return (
+        <div className ='headerWebsite'>
+            <header className ='navbarWebsite' >
+                <div>
+                    <p>Easynoonoo</p>
+                </div>
+                                
+                <input type='button' value = 'Sign in/Log in'/>
+            </header>
+
+            
+        </div>
+    )
+}
+
+
+export default Header
