b686bd0b8907178f365561c4df556acde51739cf
Clément Héré <clement.here.tech@gmail.com>
2019-12-13 13:32:21 +0100

    SimForm file updated

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import FamilyForm from './components/FamilyForm'
 import SimForm from './components/SimForm'
 
 function App() {
   return (
     <div className="App">
-      <FamilyForm />
       <SimForm />
     </div>
   )
