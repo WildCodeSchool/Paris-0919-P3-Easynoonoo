ea28e977d659873270bb5674b304433d920300ef
Artigan <a@ww4.eu>
2020-01-06 17:47:04 +0100

    add csgRds

--- back/server.js
+++ back/server.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 const { tauxChargesEmployes } = require('./models/chargesEmployes')
 // const chargesEmployesSchema = require('./models/chargesEmployes')
 // const chargesEmployes = mongoose.model('chargesEmployes', tauxChargesEmployes, 'chargesEmployes')
 
-const port = 4000;
+const port = 4000
 
 const app = express()
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
 
 // ____________ REMOTE DB ROUTE ______________
 
-const dbRoute = "mongodb+srv://easynoonoo:easynoonoo@cluster0-mznsf.azure.mongodb.net/easynoonooDB?retryWrites=true&w=majority";
+const dbRoute =
+  'mongodb+srv://easynoonoo:easynoonoo@cluster0-mznsf.azure.mongodb.net/easynoonooDB?retryWrites=true&w=majority'
 
 // ____________ CHECK YOUR CONNECTION TO MONGO DB REMOTE ______________
 
-mongoose.connect(dbRoute, { useNewUrlParser: true })
-  .then(() => console.log("DB connected"))
-  .catch(error => console.log(error));
+mongoose
+  .connect(dbRoute, { useNewUrlParser: true })
+  .then(() => console.log('DB connected'))
+  .catch(error => console.log(error))
 
 // ____________ QUERY METHOD ______________
 
@@ -40,29 +42,58 @@ mongoose.connect(dbRoute, { useNewUrlParser: true })
 /* -------------- GET TAUX INFOS ----------------- */
 
 /* -------------- TAUX EMPLOYES ----------------- */
-app.post('/api/taux/employes', function (req, res) {
-  tauxChargesEmployes.find({ 'dateDebutAnnee': req.body.dateDebutAnnee }, function (err, taux) {
+app.post('/api/taux/employes', function(req, res) {
+  tauxChargesEmployes.find(
+    { dateDebutAnnee: req.body.dateDebutAnnee },
+    function(err, taux) {
+      console.log('req.body =>', req.body.dateDebutAnnee)
 
-    console.log('req.body =>', req.body.dateDebutAnnee);
+      const arrayTr = []
 
-    const arrayTr = []
-    taux.map(val => {
-      if (req.body.trancheA) {
-        console.log('ici if');
-        arrayTr.push(val.IrcemRetraiteComplementaireTrA, val.CegTrA, 0)
-      }
-      else {
-        arrayTr.push(val.IrcemRetraiteComplementaireTrB, val.CegTrB, val.CetTrB)
-      }
+      /*--------------CALCUL SALAIRE BRUT-------------*/
       const tauxHeuresSupp = 1.25
-      const heuresMensuelles = Math.ceil( req.body.heuresHebdo * (52/12))
-      const heuresMensuellesMajorees = Math.ceil((req.body.heuresSup) * (52/12))
-      let salaireBrut = ((heuresMensuelles * req.body.tauxHoraire) + (heuresMensuellesMajorees * req.body.tauxHoraire * tauxHeuresSupp))
-      console.log('test', salaireBrut)
-      res.status(200).send({salaireBrut})
+      const heuresMensuelles = Math.ceil(
+        req.body.heuresHebdo * (52 / 12),
+      )
+      const heuresMensuellesMajorees = Math.ceil(
+        req.body.heuresSup * (52 / 12),
+      )
+      let salaireBrutMensuel =
+        heuresMensuelles * req.body.tauxHoraire +
+        heuresMensuellesMajorees *
+          req.body.tauxHoraire *
+          tauxHeuresSupp
+      console.log('test', salaireBrutMensuel)
+      res.status(200).send({ salaireBrutMensuel })
+      /*--------------------------------------------------*/
+      /*--------------CALCUL ASSIETTE CSG RDS-------------*/
+      const brutAnnuel = salaireBrutMensuel * 12
+      const PMSS = 3377
+      let assietteCsgRds =
+        98.25 * 0.01 * Math.min(4 * PMSS, brutAnnuel) +
+        Math.max(0, brutAnnuel - 4 * PMSS)
+      console.log('asiette', assietteCsgRds)
+
+      /*--------------------------------------------------*/
+      taux.map(val => {
+        if (req.body.trancheA) {
+          console.log('ici if')
+          arrayTr.push(
+            val.IrcemRetraiteComplementaireTrA,
+            val.CegTrA,
+            0,
+          )
+        } else {
+          arrayTr.push(
+            val.IrcemRetraiteComplementaireTrB,
+            val.CegTrB,
+            val.CetTrB,
+          )
+        }
       })
-  });
-});
+    },
+  )
+})
 
 // _______ APP LISTEN _______
 
@@ -72,9 +103,6 @@ app.listen(port, err => {
   }
   console.log(`server is listening on ${port}`)
 })
-
-
-
 
 // _____________ INSERT INTO DB _____________________
 
