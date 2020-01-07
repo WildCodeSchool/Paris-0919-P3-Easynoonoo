acb4399fd68e0553ad87f87c03611ef209c2a2a2
Alexis Blanchard <alexis.blanchard0@gmail.com>
2020-01-07 17:29:55 +0100

    charges calc added

--- back/server.js
+++ back/server.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 const { tauxChargesEmployes } = require('./models/chargesEmployes')
-// const chargesEmployesSchema = require('./models/chargesEmployes')
-// const chargesEmployes = mongoose.model('chargesEmployes', tauxChargesEmployes, 'chargesEmployes')
 
 const port = 4000
 
@@ -25,30 +23,11 @@ mongoose
   .then(() => console.log('DB connected'))
   .catch(error => console.log(error))
 
-// ____________ QUERY METHOD ______________
-
-// mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-// mongoose.connection.once('open', function () {
-//   mongoose.connection.db.collection("easynoonooCollection", function (err, collection) {
-
-//     //function to request collection in mongo DB
-
-//     collection.find({ name: 'card' }).toArray(function (err, data) {
-//       console.log(data);
-//     })
-//   });
-// });
-
-/* -------------- GET TAUX INFOS ----------------- */
-
 /* -------------- TAUX EMPLOYES ----------------- */
 app.post('/api/taux/employes', function(req, res) {
   tauxChargesEmployes.find(
     { dateDebutAnnee: req.body.dateDebutAnnee },
     function(err, taux) {
-      console.log('req.body =>', req.body.dateDebutAnnee)
-
-      const arrayTr = []
 
       /*--------------CALCUL SALAIRE BRUT-------------*/
       const tauxHeuresSupp = 1.25
@@ -63,34 +42,111 @@ app.post('/api/taux/employes', function(req, res) {
         heuresMensuellesMajorees *
           req.body.tauxHoraire *
           tauxHeuresSupp
-      console.log('test', salaireBrutMensuel)
-      res.status(200).send({ salaireBrutMensuel })
-      /*--------------------------------------------------*/
-      /*--------------CALCUL ASSIETTE CSG RDS-------------*/
-      const brutAnnuel = salaireBrutMensuel * 12
-      const PMSS = 3377
-      let assietteCsgRds =
-        98.25 * 0.01 * Math.min(4 * PMSS, brutAnnuel) +
-        Math.max(0, brutAnnuel - 4 * PMSS)
-      console.log('asiette', assietteCsgRds)
+      brutMensuelFamilleA = req.body.repartitionFamille * salaireBrutMensuel
+      brutMensuelFamilleB = (1 - req.body.repartitionFamille) * salaireBrutMensuel
 
       /*--------------------------------------------------*/
+
+
+      /*--------------CALCUL ASSIETTE CSG RDS-------------*/
+      const PMSS = 3377
+      let assietteCsgRdsMensuel =
+        98.25 * 0.01 * Math.min(4 * PMSS, salaireBrutMensuel) +
+        Math.max(0, salaireBrutMensuel - 4 * PMSS)
+
+      let assietteCsgRdsHoraire = 98.25 * 0.01 * req.body.tauxHoraire 
+
+      /*--------------------------------------------------*/
+
+      /*--------------CALCUL SALAIRE NET-------------*/
+
+      const arrayTr = []
       taux.map(val => {
-        if (req.body.trancheA) {
-          console.log('ici if')
+        if (req.body.trancheA && req.body.alsaceMoselle) {
           arrayTr.push(
             val.IrcemRetraiteComplementaireTrA,
             val.CegTrA,
             0,
+            val.cotisationSupplementaireAlsaceMoselle
           )
-        } else {
+        } else if(req.body.trancheA && req.body.alsaceMoselle == false) {
+          arrayTr.push(
+            val.IrcemRetraiteComplementaireTrA,
+            val.CegTrA,
+            0,
+            0
+          )
+        }
+        else if(req.body.trancheB && req.body.alsaceMoselle) {
+          arrayTr.push(
+            val.IrcemRetraiteComplementaireTrA,
+            val.CegTrA,
+            0,
+            val.cotisationSupplementaireAlsaceMoselle
+          )
+        }
+        else {
           arrayTr.push(
             val.IrcemRetraiteComplementaireTrB,
             val.CegTrB,
             val.CetTrB,
+            0
           )
         }
+        let netHoraire = req.body.tauxHoraire -
+          ((req.body.tauxHoraire * 0.01 *(
+            val.maladieMaterniteInvaliditeDeces + 
+            val.assuranceVieillesseDeplafonnee +
+            val.vieillessePlafonnee +
+            arrayTr[3] +
+            arrayTr[0] +
+            arrayTr[1] +
+            arrayTr[2] +
+            val.assuranceChomage +
+            val.IrcemPrevoyance)) 
+          + (assietteCsgRdsHoraire * 0.01 * (
+            val.CsgDeductible +
+            val.CsgNonDeductible +
+            val.CrdsNonDeductible)))
+
+          let chargesTotal = 
+          (salaireBrutMensuel * 0.01 *(
+            val.maladieMaterniteInvaliditeDeces + 
+            val.assuranceVieillesseDeplafonnee +
+            val.vieillessePlafonnee +
+            arrayTr[3] +
+            arrayTr[0] +
+            arrayTr[1] +
+            arrayTr[2] +
+            val.assuranceChomage +
+            val.IrcemPrevoyance)) 
+          + (assietteCsgRdsMensuel * 0.01 * (
+            val.CsgDeductible +
+            val.CsgNonDeductible +
+            val.CrdsNonDeductible)) 
+          + (heuresMensuellesMajorees * tauxHeuresSupp * req.body.tauxHoraire *(0.01 * val.exonerationDesCotisations))
+        
+        let netMensuelTotal = salaireBrutMensuel - chargesTotal
+        let netMensuelFamilleA = (netMensuelTotal * req.body.repartitionFamille)
+        let netMensuelFamilleB = (netMensuelTotal * (1- req.body.repartitionFamille))
+        let brutAnnuelTotal = salaireBrutMensuel * 12
+        let netAnnuelTotal = netMensuelTotal * 12
+
+        res.status(200).send({
+          "brutMensuelFamilleA" : brutMensuelFamilleA,
+          "netMensuelFamilleA" : netMensuelFamilleA,
+          "brutMensuelFamilleB" : brutMensuelFamilleB,
+          "netMensuelFamilleB" : netMensuelFamilleB,
+          "brutHoraireTotal" : req.body.tauxHoraire,
+          "netHoraireTotal" : netHoraire,
+          "netMensuelTotal" : netMensuelTotal, 
+          "brutMensuelTotal" : salaireBrutMensuel,
+          "netAnnuelTotal" : netAnnuelTotal, 
+          "brutAnnuelTotal" : brutAnnuelTotal,  
+        })
       })
+
+      /*--------------------------------------------------*/
     },
   )
 })
@@ -103,30 +159,3 @@ app.listen(port, err => {
   }
   console.log(`server is listening on ${port}`)
 })
-
-// _____________ INSERT INTO DB _____________________
-
-// async function createTranche(maladieMaterniteInvaliditeDeces) {
-//   return new tauxChargesEmployes({
-//     maladieMaterniteInvaliditeDeces
-//   }).save()
-// }
-
-// async function findTranche(maladieMaterniteInvaliditeDeces) {
-//   return await tauxChargesEmployes.findOne({ maladieMaterniteInvaliditeDeces })
-// }
-
-// ; (async () => {
-//   const connector = mongoose.connect(dbRoute)
-//   const maladieMaterniteInvaliditeDeces = process.argv[2].split('=')[1]
-
-//   let user = await connector.then(async () => {
-//     return findTranche(maladieMaterniteInvaliditeDeces)
-//   })
-
-//   if (!user) {
-//     user = await createTranche(maladieMaterniteInvaliditeDeces)
-//   }
-
-//   console.log('COLLECTION', user)
-//   process.exit(0)})
