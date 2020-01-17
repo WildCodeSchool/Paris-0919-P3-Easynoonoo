e327b7593c371f2edaa34a6e9bb245e1bd89caf7
Artigan <a@ww4.eu>
2020-01-17 17:56:07 +0100

    bootstrap form

--- front/src/components/SimForm.js
+++ front/src/components/SimForm.js
@@ -1,44 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import "./SimForm.css";
+import './SimForm.css'
 
 const SimForm = () => {
   // initialize value to the one in the localstorage in the first render
   const initialAnswer1 = () =>
-    Number(window.localStorage.getItem("answersSim1")) || 0;
+    Number(window.localStorage.getItem('answersSim1')) || 0
   const initialAnswer2 = () =>
-    Number(window.localStorage.getItem("answersSim2")) || 0;
-  const initialAnswer3 = () => window.localStorage.getItem("answersSim3") || 0;
-  const initialAnswer4 = () => window.localStorage.getItem("answersSim4") || 0;
-  const initialAnswer5 = () => window.localStorage.getItem("answersSim5") || 0;
-  const initialAnswer6 = () => window.localStorage.getItem("answersSim6") || 0;
-  const initialAnswer7 = () => window.localStorage.getItem("answersSim7") || 0;
-  const initialAnswer8 = () => window.localStorage.getItem("answersSim8") || 0;
-  const initialAnswer9 = () => window.localStorage.getItem("answersSim9") || 0;
+    Number(window.localStorage.getItem('answersSim2')) || 0
+  const initialAnswer3 = () =>
+    window.localStorage.getItem('answersSim3') || 0
+  const initialAnswer4 = () =>
+    window.localStorage.getItem('answersSim4') || 0
+  const initialAnswer5 = () =>
+    window.localStorage.getItem('answersSim5') || 0
+  const initialAnswer6 = () =>
+    window.localStorage.getItem('answersSim6') || 0
+  const initialAnswer7 = () =>
+    window.localStorage.getItem('answersSim7') || 0
+  const initialAnswer8 = () =>
+    window.localStorage.getItem('answersSim8') || 0
+  const initialAnswer9 = () =>
+    window.localStorage.getItem('answersSim9') || 0
 
   // state en hook pour les réponses
 
-  const [answersSim1, setAnswersSim1] = useState(initialAnswer1);
-  const [answersSim2, setAnswersSim2] = useState(initialAnswer2);
-  const [answersSim3, setAnswersSim3] = useState(initialAnswer3);
-  const [answersSim4, setAnswersSim4] = useState(initialAnswer4);
-  const [answersSim5, setAnswersSim5] = useState(initialAnswer5);
-  const [answersSim6, setAnswersSim6] = useState(initialAnswer6);
-  const [answersSim7, setAnswersSim7] = useState(initialAnswer7);
-  const [answersSim8, setAnswersSim8] = useState(initialAnswer8);
-  const [answersSim9, setAnswersSim9] = useState(initialAnswer9);
+  const [answersSim1, setAnswersSim1] = useState(initialAnswer1)
+  const [answersSim2, setAnswersSim2] = useState(initialAnswer2)
+  const [answersSim3, setAnswersSim3] = useState(initialAnswer3)
+  const [answersSim4, setAnswersSim4] = useState(initialAnswer4)
+  const [answersSim5, setAnswersSim5] = useState(initialAnswer5)
+  const [answersSim6, setAnswersSim6] = useState(initialAnswer6)
+  const [answersSim7, setAnswersSim7] = useState(initialAnswer7)
+  const [answersSim8, setAnswersSim8] = useState(initialAnswer8)
+  const [answersSim9, setAnswersSim9] = useState(initialAnswer9)
 
   //store the data in local storage
   useEffect(() => {
-    window.localStorage.setItem("answersSim1", answersSim1);
-    window.localStorage.setItem("answersSim2", answersSim2);
-    window.localStorage.setItem("answersSim3", answersSim3);
-    window.localStorage.setItem("answersSim4", answersSim4);
-    window.localStorage.setItem("answersSim5", answersSim5);
-    window.localStorage.setItem("answersSim6", answersSim6);
-    window.localStorage.setItem("answersSim7", answersSim7);
-    window.localStorage.setItem("answersSim8", answersSim8);
-    window.localStorage.setItem("answersSim9", answersSim9);
+    window.localStorage.setItem('answersSim1', answersSim1)
+    window.localStorage.setItem('answersSim2', answersSim2)
+    window.localStorage.setItem('answersSim3', answersSim3)
+    window.localStorage.setItem('answersSim4', answersSim4)
+    window.localStorage.setItem('answersSim5', answersSim5)
+    window.localStorage.setItem('answersSim6', answersSim6)
+    window.localStorage.setItem('answersSim7', answersSim7)
+    window.localStorage.setItem('answersSim8', answersSim8)
+    window.localStorage.setItem('answersSim9', answersSim9)
   }, [
     answersSim1,
     answersSim2,
@@ -48,14 +55,13 @@ const SimForm = () => {
     answersSim6,
     answersSim7,
     answersSim8,
-    answersSim9
-  ]); //callback run only the answers change
+    answersSim9,
+  ]) //callback run only the answers change
 
   useEffect(() => {
-    handleQuestion9();
-    console.log("bonjour");
-  }, [answersSim8]);
-
+    handleQuestion9()
+    console.log('bonjour')
+  }, [answersSim8])
 
   const handleQuestion9 = () => {
     if (
@@ -64,78 +70,94 @@ const SimForm = () => {
     ) {
       return (
         <div>
-          <select>
+          <select class="form-control">
             <option>Inférieures ou égales à 20 755 €</option>
             <option>
-              Supérieures à 20 755 € et inférieures ou égales à 46 123 €
+              Supérieures à 20 755 € et inférieures ou égales à
+              46 123 €
             </option>
             <option>Supérieures à 46 123 €</option>
           </select>
         </div>
-      );
+      )
     } else if (
       (answersSim6 == 2 && answersSim7 <= 3) ||
       (answersSim6 == 2 && answersSim7 >= 3)
     ) {
       return (
         <div>
-          <select>
+          <select class="form-control">
             <option>Inférieures ou égales à 23 701 €</option>
             <option>
-              Supérieures à 23 701 € et inférieures ou égales à 52 670 €
+              Supérieures à 23 701 € et inférieures ou égales à
+              52 670 €
             </option>
             <option>Supérieures à 52 670 €</option>
           </select>
         </div>
-      );
-
-    } else if ((answersSim6 == 3 && answersSim7 <= 3) || (answersSim6 == 3 && answersSim7) >= 3) {
+      )
+    } else if (
+      (answersSim6 == 3 && answersSim7 <= 3) ||
+      (answersSim6 == 3 && answersSim7) >= 3
+    ) {
       return (
         <div>
-          <select>
+          <select class="form-control">
             <option>Inférieures ou égales à 26 647 €</option>
-            <option>Supérieures à 26 647 € et inférieures ou égales à 59 217 €</option>
+            <option>
+              Supérieures à 26 647 € et inférieures ou égales à
+              59 217 €
+            </option>
             <option>Supérieures à 59 217 €</option>
           </select>
         </div>
-      );
-
-    } else if ((answersSim6 == 4 && answersSim7 <= 3) || (answersSim6 == 4 && answersSim7 >= 3)) {
+      )
+    } else if (
+      (answersSim6 == 4 && answersSim7 <= 3) ||
+      (answersSim6 == 4 && answersSim7 >= 3)
+    ) {
       return (
         <div>
-          <select>
+          <select class="form-control">
             <option>Inférieures ou égales à 29 593 €</option>
-            <option>Supérieures à 29 593 € et inférieures ou égales à 65 764 €</option>
+            <option>
+              Supérieures à 29 593 € et inférieures ou égales à
+              65 764 €
+            </option>
             <option>Supérieures à 65 764 €</option>
           </select>
         </div>
-      );
+      )
     }
-  };
+  }
 
   return (
-    <div className="simFormParent">
+    <div className="container">
       <h2>Simulation de salaire</h2>
-      <div className="simForm">
-        <div className="simFormNumberInput">
+      <div class="form-group">
+        <div>
           <label>
-            1. Quel est le temps de travail effectif hebdomadaire de votre garde
-            d'enfant(s) ?
+            1. Quel est le temps de travail effectif hebdomadaire de
+            votre garde d'enfant(s) ?
           </label>
           <input
+            class="form-control"
             type="number"
             value={answersSim1}
-            onChange={e => setAnswersSim1(parseInt(e.target.value, 10))}
+            onChange={e =>
+              setAnswersSim1(parseInt(e.target.value, 10))
+            }
             min="1"
             max="100"
           />
         </div>
 
-        <div className="simFormNumberInput">
+        <div>
           <label for="region-select">
             2. Dans quelle région habitez-vous ?
           </label>
           <select
+            class="form-control"
             name="region"
             id="region-select"
             onChange={e => setAnswersSim2(e.target.value)}
@@ -152,59 +174,69 @@ const SimForm = () => {
 
         <div className="simFormNumberInput">
           <label>
-            3. Quel est le salaire brut horaire de votre garde d'enfant(s) ?
+            3. Quel est le salaire brut horaire de votre garde
+            d'enfant(s) ?
           </label>
           <input
+            class="form-control"
             type="number"
             value={answersSim3}
-            onChange={e => setAnswersSim3(parseFloat(e.target.value, 10))}
+            onChange={e =>
+              setAnswersSim3(parseFloat(e.target.value, 10))
+            }
             min="10"
             max="100"
           />
         </div>
 
-        <p className="question4">4. La garde est-elle partagée ?</p>
-        <div className="radio">
-          <label>
-            <input
-              type="radio"
-              className="checked"
-              value="true"
-              checked={answersSim4 === "true"}
-              onChange={e => setAnswersSim4(e.target.value)}
-            />
-            Oui
-          </label>
+        <p>4. La garde est-elle partagée ?</p>
+        <div className="input-group">
+          <div>
+            <label class="input-group-text">
+              <input
+                type="radio"
+                value="true"
+                checked={answersSim4 === 'true'}
+                onChange={e => setAnswersSim4(e.target.value)}
+              />
+              Oui
+            </label>
+          </div>
+
+          <div>
+            <label className="input-group-text">
+              <input
+                type="radio"
+                className="checked"
+                value="false"
+                checked={answersSim4 === 'false'}
+                onChange={e => setAnswersSim4(e.target.value)}
+              />
+              Non
+            </label>
+          </div>
         </div>
 
-        <div className="radio">
-          <label>
-            <input
-              type="radio"
-              className="checked"
-              value="false"
-              checked={answersSim4 === "false"}
-              onChange={e => setAnswersSim4(e.target.value)}
-            />
-            Non
-          </label>
-        </div>
-
-        {answersSim4 === "true" ? (
+        {answersSim4 === 'true' ? (
           <div className="simFormNumberInput">
             <label>
-              5. Quelle part du coût de la garde allez-vous supporter (en %) ?
+              5. Quelle part du coût de la garde allez-vous supporter
+              ?
             </label>
             <input
-              type="number"
+              type="range"
               value={answersSim5}
               onChange={e => setAnswersSim5(parseInt(e.target.value))}
-              min="1"
+              class="custom-range"
+              min="0"
               max="100"
-            />
+              step="10"
+              id="customRange3"
+            ></input>
+            {answersSim5} %
           </div>
         ) : (
-          ""
+          ''
         )}
 
         <div className="simFormNumberInput">
@@ -212,6 +244,7 @@ const SimForm = () => {
             6. Combien d'enfants avez-vous à charge ?
           </label>
           <select
+            class="form-control"
             name="childs"
             id="child-select"
             onChange={e => setAnswersSim6(e.target.value)}
@@ -227,8 +260,11 @@ const SimForm = () => {
         </div>
 
         <div className="simFormNumberInput">
-          <label>7. Quel est l'âge du plus jeune enfant gardé ?</label>
+          <label>
+            7. Quel est l'âge du plus jeune enfant gardé ?
+          </label>
           <input
+            class="form-control"
             type="number"
             value={answersSim7}
             onChange={e => setAnswersSim7(parseInt(e.target.value))}
@@ -237,51 +273,56 @@ const SimForm = () => {
           />
         </div>
 
-        <p className="question8">8. Elevez-vous seul.e votre enfant ?</p>
-        <div className="radio">
-          <label>
-            <input
-              type="radio"
-              className="checked"
-              value="true"
-              checked={answersSim8 === "true"}
-              onChange={e => setAnswersSim8(e.target.value)}
-            />
-            Oui
-          </label>
-        </div>
+        <p className="question8">
+          8. Elevez-vous seul.e votre enfant ?
+        </p>
+        <div className="input-group">
+          <div>
+            <label class="input-group-text">
+              <input
+                type="radio"
+                className="checked"
+                value="true"
+                checked={answersSim8 === 'true'}
+                onChange={e => setAnswersSim8(e.target.value)}
+              />
+              Oui
+            </label>
+          </div>
 
-        <div className="radio">
-          <label>
-            <input
-              type="radio"
-              className="checked"
-              value="false"
-              checked={answersSim8 === "false"}
-              onChange={e => setAnswersSim8(e.target.value)}
-            />
-            Non
-          </label>
+          <div className="radio">
+            <label class="input-group-text">
+              <input
+                type="radio"
+                className="checked"
+                value="false"
+                checked={answersSim8 === 'false'}
+                onChange={e => setAnswersSim8(e.target.value)}
+              />
+              Non
+            </label>
+          </div>
         </div>
 
         {answersSim8 === 'true' || answersSim8 === 'false' ? (
           <div className="simFormNumberInput">
             <label for="salarySelect">
-            9. Quels sont les revenus nets mensuels du foyer ?
+              9. Quels sont les revenus nets mensuels du foyer ?
             </label>
-            <div onChange={e => setAnswersSim9(e.target.value)} >
-            { handleQuestion9() }
+            <div onChange={e => setAnswersSim9(e.target.value)}>
+              {handleQuestion9()}
             </div>
-
           </div>
         ) : (
-          ""
+          ''
         )}
 
-        <Link to='/'><p className="simFormReturn">Retour aux simulateurs</p></Link>
+        <Link to="/">
+          <p className="simFormReturn">Retour aux simulateurs</p>
+        </Link>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default SimForm;
+export default SimForm
