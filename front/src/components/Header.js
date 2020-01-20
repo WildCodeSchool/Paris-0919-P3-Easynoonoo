82224cbdad71bd8704c4aca79c33199c83f72996
TlZeyn <jacqueline.nguyen.41@gmail.com>
2020-01-20 10:33:56 +0100

    changed noonoo1 to svg

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -5,8 +5,8 @@ import logo from '../assets/noonoo2.svg'
 const Header = () => {
 
     return (
-        <div className ='container headerWebsite'>
-            <nav className ='navbarWebsite' >
+        <div className ='headerWebsite'>
+            <nav  className ='navbarWebsite' >
                 <div>
                     <img className ='headerLogo' src ={logo} alt = 'logo' />
                 </div>                                
